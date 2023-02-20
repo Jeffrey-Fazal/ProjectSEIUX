030c75e012baffee7d6b1f8075a70ece44262c6f
Jeffrey Fazal <jeffreyfazal@gmail.com>
2023-02-20 12:43:57 +1100

    first commit

---
+++ folderstructure.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Create the subdirectories
+mkdir css
+mkdir js
+mkdir images
+
+# Create the files
+touch index.html
+touch css/styles.css
+touch js/main.js
+touch images/pet-drone.png
